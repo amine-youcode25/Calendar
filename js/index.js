30eba2fa13809bbd7d5a5349895b4f8cf5884af1
AMINE <amineelhailaa@gmail.com>
2025-10-30 13:11:24 +0100

    finish delete modify and color type of reservation

--- js/index.js
+++ js/index.js
@@ -5,13 +5,13 @@ const formsection = document.getElementById('form_user');
 let day = 0;
 
 
-function formOnOff(){
+function formOnOff() {
     formsection.classList.toggle('tgFunction');
 }
 
 
-function getForm(){
-    const data={
+function getForm() {
+    const data = {
         client: document.getElementById('clientName').value,
         start: document.getElementById('startTime').value,
         end: document.getElementById('endTime').value,
@@ -19,7 +19,6 @@ function getForm(){
     }
     return data;
 }
-
 
 
 gdSection.addEventListener('click', function (detector) {
@@ -33,18 +32,13 @@ gdSection.addEventListener('click', function (detector) {
 // <div class="fs-6 bg-warning rounded-1 d-flex align-items-center justify-content-center p-0" >hello mate</div>
 
 
-
-
-
-    formcont.addEventListener('submit', function (e) {
-        e.preventDefault();
-        getForm();
-        add(day,getForm());
-        formcont.reset();
-        formOnOff();
-    });
-
-
+formcont.addEventListener('submit', function (e) {
+    e.preventDefault();
+    getForm();
+    add(day, getForm());
+    formcont.reset();
+    formOnOff();
+});
 
 
 //c oblige poiur que l'utilisateur click outside de la form pour la ferme
@@ -57,36 +51,61 @@ formsection.addEventListener('click', function (e) {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-function add(whichday,getform) {
+function add(whichday, getform) {
     const reservationDiv = document.createElement('div');
-    reservationDiv.className = 'fs-6 bg-warning rounded-1 d-flex align-items-center justify-content-center p-0'
-    reservationDiv.innerHTML= `<span>${getform.client}</span>
+    reservationDiv.className = 'fs-6 rounded-1 d-flex align-items-center justify-content-center p-0'
+
+
+
+    //styling colors need to add text color too , je peux ajouter une animation sur vip
+    if (getform.type === "standard"){
+        reservationDiv.style.backgroundColor = 'green';
+    }
+    else if (getform.type === "birthday"){
+        reservationDiv.style.backgroundColor = 'blue';
+    }
+    else if (getform.type === "group"){
+        reservationDiv.style.backgroundColor = 'white';
+    }
+    else if (getform.type === "vip"){
+        reservationDiv.style.backgroundColor = 'yellow';
+    }
+
+
+    reservationDiv.innerHTML = `<span>${getform.client}</span>
         <span>${getform.start} - ${getform.end}</span>
         <div>
             <button class="btn-edit"><i class="fa-solid fa-pen-to-square"></i></button>
             <button class="btn-remove"><i class="fa-solid fa-trash"></i></button>
         </div>`;
-    reservationDiv.querySelector('.btn-remove').addEventListener('click', function() {
+
+
+    reservationDiv.querySelector('.btn-remove').addEventListener('click', function (e) {
+        e.stopPropagation()
         reservationDiv.remove();
     });
 
-
-    reservationDiv.querySelector('.btn-edit').addEventListener('click', function() {
+    reservationDiv.querySelector('.btn-edit').addEventListener('click', function (e) {
+        e.stopPropagation()
         // Show form with existing data
+        console.log("u clicked modify")
+        reservationDiv.remove();
+        day = whichday;
+       document.getElementById('clientName').value = getform.client
+     document.getElementById('startTime').value= getform.start
+    document.getElementById('endTime').value=getform.end
+        document.getElementById('reservationType').value = getform.type
+        formOnOff()
+        console.log("skipped toggle")
 
+        // document.getElementById('clientName').value = getform.client
+        // document.getElementById('startTime').value = getform.start
+        // document.getElementById('endTime').value = getform.end
+        // document.getElementById('reservationType').value = getform.type
     });
     whichday.appendChild(reservationDiv);
 }
+
 
 
 //if user press anywhere than grid section
